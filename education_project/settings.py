ba2d3d7a076660dfaacca3003d45425b34320ea9
dankNecessities <sam.olwe@gmail.com>
2020-02-17 04:41:45 +0300

    Added logo and video

--- education_project/settings.py
+++ education_project/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR + '/frontend/media/' 
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
