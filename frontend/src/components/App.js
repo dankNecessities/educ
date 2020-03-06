6eda0d12d852e935e4cd88a5d242d84fe3010210
dankNecessities <sam.olwe@gmail.com>
2020-03-06 20:02:34 +0300

    Panic push

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import Title;
 //import Button from 'react-bootstrap/Button';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
